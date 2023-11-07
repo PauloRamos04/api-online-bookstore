a3aa66af2e51f77ea37d95582751acac27ae40e3
PauloRamos04 <pauloramos.04@hotmail.com>
2023-11-06 22:01:24 -0300

    Subindo mais funcionalidades para User e Publisher

--- src/main/java/online/bookstore/api/controller/PublisherController.java
+++ src/main/java/online/bookstore/api/controller/PublisherController.java
@@ -6,10 +6,9 @@ import online.bookstore.api.domain.publisher.DadosCadastroPublisher;
 import online.bookstore.api.domain.publisher.Publisher;
 import online.bookstore.api.domain.publisher.PublisherRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/autores")
@@ -19,6 +18,12 @@ public class PublisherController {
     @Autowired
     private PublisherRepository publisherRepository;
 
+
+    @GetMapping()
+    public List<Publisher> listarPublisher(){return publisherRepository.findAll();}
+
+    @DeleteMapping()
+    public void excluirPublisher(@PathVariable Long id){publisherRepository.deleteById(id);}
     @PostMapping
     public void cadastrar(@RequestBody @Valid DadosCadastroPublisher dados){
         publisherRepository.save(new Publisher(dados));
